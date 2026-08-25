c63cef580c18e39c9be96e5aaca56af93eab3bbc
Eysa Lee <eysa.lee@gmail.com>
2026-08-25 15:15:39 -0400

    removing things we're not using

--- project/project3/project3_check.py
+++
@@ -1,8 +0,0 @@
-from datascience import *
-import numpy as np
-
-def correctness_check_5(accuracy):
-    try:
-        return np.isclose(np.round(accuracy, 3), 0.9)
-    except Exception:
-        return False
